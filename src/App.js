f0745d965381c146e8099f1e4eaf27109f002848
Nathan Rogan <rolorogan@gmail.com>
2023-11-24 09:33:18 +0000

    remove base url logic

--- src/App.js
+++ src/App.js
@@ -9,23 +9,6 @@ import BlogArticles from "./Blog/BlogArticles";
 import BlogArticle, { loader as blogLoader, } from "./Blog/BlogArticle";
 import ErrorPage from "./error-page";
 
-// ensure app works on custom content page in cms by using the path location
-const pathname = window.location.pathname;
-
-console.log(pathname, 'orign path name')
-
-function removePathIncludingArticle(url) {
-  // Use a regular expression to match everything including and after /article
-  const regex = /\/article(\/.*)?/;
-  
-  // Replace the matched part with an empty string
-  const result = url.replace(regex, '');
-  
-  console.log(result, 'new path name')
-
-  return result;
-}
-
 const container = document.getElementById("root");
 
 const root = createRoot(container);

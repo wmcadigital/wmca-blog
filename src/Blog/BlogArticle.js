c28268fcafbac96b6f6dcdb7f082cf622ea79bb1
Restyled.io <commits@restyled.io>
2024-01-05 16:43:48 +0000

    Restyled by clang-format

--- src/Blog/BlogArticle.js
+++ src/Blog/BlogArticle.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { useLoaderData, Link } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link, useLoaderData} from "react-router-dom";
+
 import getBlogArticle from "../api/getBlogArticle";
+import formatDate from "../helpers/formatDate";
 import ScrollToTop from "../helpers/ScrollToTop";
-import { useState, useEffect } from "react";
+
+import AccordionComponent from "./AccordionComponent";
 // import BlogBody from "./BlogBody";
 
 import Banner from "./Banner";
-import formatDate from "../helpers/formatDate";
-import VideoComponent from "./VideoComponent";
-import TextComponent from "./TextComponent";
+import Breadcrumb from "./Breadcrumb";
 import ImageComponent from "./ImageComponent";
 import SidebarCardComponent from "./SidebarCardComponent";
-import AccordionComponent from "./AccordionComponent";
-import Breadcrumb from "./Breadcrumb";
+import TextComponent from "./TextComponent";
+import VideoComponent from "./VideoComponent";
 
-export async function loader({ params }) {
+export async function loader({params}) {
   const article = await getBlogArticle(params.articleTitle);
-  return { article };
+  return {article};
 }
 
 const BlogArticle = () => {
--- src/Blog/BlogArticleLink.js
+++ src/Blog/BlogArticleLink.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { useState, useEffect } from "react";
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react'
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 import formatDate from "../helpers/formatDate";
+
 // import extractExcerpt from "../helpers/extractExcerpt";
 
 const BlogArticleLink = ({
@@ -23,53 +24,52 @@ const BlogArticleLink = ({
 
   const handleAuthor = (event, item) => {
     event.preventDefault();
-    let selectedAuthor = [item]
-    setFilter({ ...filter, author: selectedAuthor });
-
+    let selectedAuthor = [ item ]
+    setFilter({...filter, author : selectedAuthor});
   };
 
   const handleTopics = (event, item) => {
     event.preventDefault();
-    let selectedTopic = [item]
-    setFilter({ ...filter, topics: selectedTopic });
+    let selectedTopic = [ item ]
+    setFilter({...filter, topics : selectedTopic});
   };
 
-  const routePath = (path) => {
-    const regex = new RegExp(`/blog(/)?`);
-    const result = path.replace(regex, '');
-    return result;
-  }
+  const routePath =
+      (path) => {
+        const regex = new RegExp(`/blog(/)?`);
+        const result = path.replace(regex, '');
+        return result;
+      }
 
   useEffect(() => {
-  // match check to mark which topics should be linked
-  let blogTopics = window?.setTopics.topics;
+    // match check to mark which topics should be linked
+    let blogTopics = window?.setTopics.topics;
 
-  const topics = tags.map((el1) => ({
-    name: el1,
-    match: blogTopics.some((el2) => el2 === el1),
-  }))
+    const topics = tags.map((el1) => ({
+                              name : el1,
+                              match : blogTopics.some((el2) => el2 === el1),
+                            }))
 
-  setTopics(topics);
-
-  }, [name, tags]);
+    setTopics(topics);
+  }, [ name, tags ]);
 
   return (
     <div className="wmcads-search-result">
       <h2 className="wmcads-m-b-sm">
-        <Link className="h2" to={{ pathname: `article/${routePath(route)}`}} >
+        <Link className="h2" to={{
+    pathname: `article/${routePath(route)}`}} >
           {name}
         </Link>
       </h2>
       <p className="wmcads-search-result__date">
         {authors?.map(function (item, index) {
-          return (
-            <React.Fragment key={index}>
-              {index > 0 && ', '}
-              <a key={`${index}`} onClick={(e) => handleAuthor(e, item.name)} onKeyUp={handleAuthor} role="link">
-                {item.name}
-              </a>
-            </React.Fragment>
-          );
+    return (<React.Fragment key = {index}>{
+        index > 0 &&
+        ', '}<a key = {`${index}`} onClick = {(e) => handleAuthor(
+                                                  e, item.name)} onKeyUp =
+                  {handleAuthor} role = "link">{
+        item.name}</a>
+            </React.Fragment>);
         })}&nbsp;-&nbsp;
         {formatDate(publishDate)}
       </p>
@@ -89,11 +89,14 @@ const BlogArticleLink = ({
               }
             </React.Fragment>
           );
-        })}
-      </p>
+        })
+}
+< /p>
       {image != "No Image" ? (
         <img
-          src={`https://cms.wmca.org.uk${image}?anchor=center&mode=crop&width=600&height=250`}
+          src={`https:/ /
+        cms.wmca.org.uk${image}
+? anchor = center &mode = crop &width = 600 &height = 250 `}
           alt=""
           className="wmcads-m-t-md"
         />

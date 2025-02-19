4c8e5f3de3d244e68ec815532e7df6ed3aeacb03
Nathan Rogan <nathanroagn@centro.org.uk>
2025-02-19 15:59:21 +0000

    add src url to img in rich text editor

--- src/Blog/TextComponent.js
+++ src/Blog/TextComponent.js
@@ -1,8 +1,14 @@
 import PropTypes from "prop-types";
 
 const TextComponent = ({ htmlContent }) => {
+  const updatedHtmlContent = htmlContent.replace(/<img[^>]+src="([^">]+)"/g, (match, p1) => {
+    const newSrc = `https://cms.wmca.org.uk/${p1}`;
+    return match.replace(p1, newSrc);
+  });
+
+  console.log(updatedHtmlContent);
   return (
-    <p dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    <p dangerouslySetInnerHTML={{ __html: updatedHtmlContent }} />
   );
 };
 
